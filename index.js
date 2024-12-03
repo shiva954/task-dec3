e8cba4150f77b7f4d7d2062160475699d20d93e6
shiva954 <orsushiva138@gmail.com>
2024-12-03 18:22:44 +0530

    meassage

---
+++ index.js
@@ -0,0 +1,10 @@
+import message from "./start.dom";
+
+const alldata=document.getElementById("data")
+const real=document.getElementsByClassName("newinput")
+const newclass=document.getElementsByClassName("ramdom")
+const ramdoms=Document.getElementsByClassName("ramdom")
+const newsname=Document.getElementsByClassName("newname")
+const highs=Document.getElementsByClassName("high")
+
+
